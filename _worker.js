f3458584c6f317f572c1e1695abd65b6720fca86
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-07-13 15:24:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -740,7 +740,7 @@ function convertToNAT64IPv6(ipv4Address) {
         return num.toString(16).padStart(2, '0');
     });
     
-    return `2001:67c:2960:6464::${hex[0]}${hex[1]}:${hex[2]}${hex[3]}`;
+    return `2602:fc59:b0:64::${hex[0]}${hex[1]}:${hex[2]}${hex[3]}`;
 }
 
 // 获取域名的IPv4地址并转换为NAT64 IPv6地址
